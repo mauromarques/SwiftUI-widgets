aa3bae948d789f64043ba1816b37359fcd1a6b63
Mauro <mauro.filho@ua.pt>
2021-03-07 16:00:38 +0000

    Initial Commit

---
+++ BTC widget/BTC_widgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  BTC_widgetApp.swift
+//  BTC widget
+//
+//  Created by Mauro Canhao on 07/03/21.
+//
+
+import SwiftUI
+
+@main
+struct BTC_widgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BTC widget/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BTC widget
+//
+//  Created by Mauro Canhao on 07/03/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
